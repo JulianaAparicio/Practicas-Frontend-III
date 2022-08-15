653e58aa6cac90640c8db2926204b85f63527d28
JulianaAparicio <juliana_aparicio@outlook.com>
2022-08-15 20:02:11 -0300

    Descargue ejercicio

---
+++ mesa-c7/practica-clase-7/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const saludos = [ "HOLA", "HOLA", "HOLA", "MUNDO" ]
+
+class App extends React.Component {
+  render() {
+    return (
+    <div>
+      
+    </div>
+    )
+  }
+}
+
+export default App;
